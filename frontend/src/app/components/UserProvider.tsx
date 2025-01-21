be55bf2cb29a3617826df29e03e7074d371b015d
Kim Dong O <kdo_1999@naver.com>
2025-01-21 11:43:54 +0900

    refactor: 회원 조회로 유저 이름 설정 #110

--- frontend/src/app/components/UserProvider.tsx
+++ frontend/src/app/components/UserProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 interface UserContextType {
     username: string;
     setUsername: (username: string) => void;
@@ -16,8 +16,22 @@ export function UserProvider({ children }: UserProviderProps) {
     const [username, setUsername] = useState<string>('');
 
     useEffect(() => {
-        const storedUsername = localStorage.getItem('username') || '';
-        setUsername(storedUsername);
+        const fetchUsername = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/members`, {
+                    cache: 'no-store',
+                    credentials: 'include',
+                });
+                const result = await response.json();
+                if (response.status === 200) {
+                    setUsername(result.data.username);
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUsername();
     }, []);
 
     return (
@@ -27,7 +41,7 @@ export function UserProvider({ children }: UserProviderProps) {
     );
 }
 
-export function useUser() {
+export function useUser(): UserContextType {
     const context = useContext(UserContext);
     if (context === undefined) {
         throw new Error('useUser must be used within a UserProvider');
